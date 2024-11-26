a65e3141d6d1487713e6ce4d161049db482540ee
nghiatrungnn <nguyennghiatrung127@gmail.com>
2024-11-26 15:24:52 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {
   database: {
-    uri: "mongodb+srv://trung:<0973112230>@cluster1.8ltwg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1" // Thay <your_password> bằng mật khẩu thực tế
+    uri: "mongodb+srv://trung98213:<db_password>@trung.21wj1.mongodb.net/?retryWrites=true&w=majority&appName=Trung" // Thay <your_password> bằng mật khẩu thực tế
   },
   server: {
     port: 5000 // Cổng server
